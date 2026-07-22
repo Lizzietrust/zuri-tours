82c1fd71d9fff5f2d4947c72cc912c496695d765
Lizzietrust <ohidaelizabeth3@gmail.com>
2026-07-22 20:37:05 +0100

    feat: refactor tour routes for improved organization and clarity

--- server/routes/tourRoutes.js
+++ server/routes/tourRoutes.js
@@ -10,18 +10,31 @@ import {
   getToursByPriceRange,
   getTopCheapTours,
   getToursByDifficulty,
+  getToursByDuration,
+  getToursByRating,
+  searchTours,
 } from "../controllers/tourController.js";
 
 const router = express.Router();
 
-router.get("/stats", getTourStats);
-router.get("/monthly-plan/:year", getMonthlyPlan);
-router.get("/top-5-cheap", getTopCheapTours);
-router.get("/price-range", getToursByPriceRange);
-router.get("/difficulty/:level", getToursByDifficulty);
+router.route("/top-5-cheap").get(getTopCheapTours);
+router.route("/top-rated").get(getToursByRating);
+router.route("/shortest").get(getToursByDuration);
+
+router.route("/stats").get(getTourStats);
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
+
+router.route("/price-range").get(getToursByPriceRange);
+router.route("/difficulty/:level").get(getToursByDifficulty);
+router.route("/search").get(searchTours);
 
 router.route("/").get(getAllTours).post(createTour);
 
-router.route("/:id").get(getTour).put(updateTour).delete(deleteTour);
+router
+  .route("/:id")
+  .get(getTour)
+  .put(updateTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 export default router;
