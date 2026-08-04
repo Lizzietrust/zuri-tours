fcd9ac2ecc825088c8f96a08900ecd1c85c5b45d
Lizzietrust <ohidaelizabeth3@gmail.com>
2026-08-04 10:37:33 +0100

    refactor: reorganize routes for better clarity and structure

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -16,14 +16,13 @@ import {
 
 const router = express.Router();
 
-// Public routes
 router.post("/register", validateUser, register);
 router.post("/login", validateAuth, login);
 router.post("/forgotpassword", forgotPassword);
 router.put("/resetpassword/:resetToken", resetPassword);
 
-// Protected routes
 router.use(protect);
+
 router.get("/me", getMe);
 router.put("/updatepassword", updatePassword);
 router.get("/logout", logout);
