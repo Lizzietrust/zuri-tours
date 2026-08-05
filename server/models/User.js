405f4b417cabb937dec8dd879e6ad89c92524c04
Lizzietrust <ohidaelizabeth3@gmail.com>
2026-08-05 09:15:11 +0100

    refactor: enhance user model with password management, account deletion, and login attempt features

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
-import mongoose from "mongoose"; // eslint-disable-line import/no-extraneous-dependencies
-import bcrypt from "bcryptjs"; // eslint-disable-line import/no-extraneous-dependencies
-import jwt from "jsonwebtoken"; // eslint-disable-line import/no-extraneous-dependencies
-import validator from "validator"; // eslint-disable-line import/no-extraneous-dependencies
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import validator from "validator";
 
 function validateNameLength(value) {
   return validator.isLength(value, { min: 2, max: 50 });
@@ -14,10 +14,10 @@ function validateEmail(value) {
 function validatePasswordStrength(value) {
   return validator.isStrongPassword(value, {
     minLength: 6,
-    minLowercase: 0,
-    minUppercase: 0,
-    minNumbers: 0,
-    minSymbols: 0,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
   });
 }
 
@@ -71,6 +71,42 @@ const userSchema = new mongoose.Schema(
       type: Date,
       default: Date.now,
     },
+    passwordChangedAt: {
+      type: Date,
+      select: false,
+    },
+    lastLogin: {
+      type: Date,
+      select: false,
+    },
+    loginAttempts: {
+      type: Number,
+      default: 0,
+      select: false,
+    },
+    lockUntil: {
+      type: Date,
+      select: false,
+    },
+    accountDeleted: {
+      type: Boolean,
+      default: false,
+      select: false,
+    },
+    accountDeletedAt: {
+      type: Date,
+      select: false,
+    },
+    tokenVersion: {
+      type: Number,
+      default: 0,
+      select: false,
+    },
+    activeSessions: {
+      type: [String],
+      default: [],
+      select: false,
+    },
   },
   {
     timestamps: true,
@@ -104,18 +140,100 @@ userSchema.pre("save", async function hashPassword(next) {
 
   this.password = await bcrypt.hash(this.password, salt);
 
+  this.passwordChangedAt = new Date();
+  this.tokenVersion = (this.tokenVersion || 0) + 1;
+
   next();
 });
 
+userSchema.methods.changedPasswordAfter = function changedPasswordAfter(
+  JWTTimestamp,
+) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10,
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  return false;
+};
+
+userSchema.methods.isAccountDeleted = function isAccountDeleted() {
+  return this.accountDeleted === true;
+};
+
+userSchema.methods.isAccountLocked = function isAccountLocked() {
+  return this.lockUntil && this.lockUntil > new Date();
+};
+
+userSchema.methods.handleFailedLogin = async function handleFailedLogin() {
+  this.loginAttempts = (this.loginAttempts || 0) + 1;
+
+  const MAX_ATTEMPTS = 5;
+  const LOCK_TIME = 30 * 60 * 1000;
+
+  if (this.loginAttempts >= MAX_ATTEMPTS) {
+    this.lockUntil = new Date(Date.now() + LOCK_TIME);
+  }
+
+  await this.save({ validateBeforeSave: false });
+};
+
+userSchema.methods.resetLoginAttempts = async function resetLoginAttempts() {
+  this.loginAttempts = 0;
+  this.lockUntil = undefined;
+  this.lastLogin = new Date();
+  await this.save({ validateBeforeSave: false });
+};
+
+userSchema.methods.softDelete = async function softDelete() {
+  this.accountDeleted = true;
+  this.accountDeletedAt = new Date();
+  this.tokenVersion = (this.tokenVersion || 0) + 1;
+  await this.save({ validateBeforeSave: false });
+};
+
+userSchema.methods.reactivate = async function reactivate() {
+  this.accountDeleted = false;
+  this.accountDeletedAt = undefined;
+  await this.save({ validateBeforeSave: false });
+};
+
 userSchema.methods.getSignedJwtToken = function getSignedJwtToken() {
-  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
+  const payload = {
+    id: this._id,
+    tokenVersion: this.tokenVersion || 0,
+  };
+
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE || "7d",
   });
 };
 
 userSchema.methods.matchPassword = function matchPassword(enteredPassword) {
   return bcrypt.compare(enteredPassword, this.password);
 };
+
+userSchema.statics.findAndVerifyUser = async function findAndVerifyUser(id) {
+  const user = await this.findById(id)
+    .select("+passwordChangedAt +accountDeleted +tokenVersion")
+    .select("+loginAttempts +lockUntil");
+
+  if (!user) return null;
+
+  if (user.accountDeleted) return null;
+
+  if (user.lockUntil && user.lockUntil > new Date()) return null;
+
+  return user;
+};
+
+userSchema.index({ accountDeleted: 1 });
+userSchema.index({ tokenVersion: 1 });
+userSchema.index({ lockUntil: 1 });
 
 const User = mongoose.model("User", userSchema);
 
